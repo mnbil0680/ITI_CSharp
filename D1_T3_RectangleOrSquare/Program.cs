50f86d0ad750942681693cdf6159ddd023964b3b
Mohamed Nabil Mohamed Abusarea <mnbil0680@gmail.com>
2025-07-09 01:15:04 +0300

    add Day1 Task3 Rectangl or Square Program

---
+++ D1_T3_RectangleOrSquare/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace D1_T3_RectangleOrSquare
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a C# program that checks if a rectangle with given length and width is a square.
+            //Created by Mohamed Nabil
+
+            Console.Write("Please Enter the Length of the Rectangle: ");
+            int Length = int.Parse(Console.ReadLine());
+
+            Console.Write("Please Enter the Width of the Rectangle: ");
+            int Width = int.Parse(Console.ReadLine());
+
+            if (Length == Width)
+            {
+                Console.WriteLine("The Rectangle is a Square");
+            }
+            else
+            {
+                Console.WriteLine("The Rectangle is not a Square");
+            }
+        }
+    }
+}
