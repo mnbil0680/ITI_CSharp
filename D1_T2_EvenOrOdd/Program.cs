9fe5ff5ee4a006ebfe65d8c395e06b3738969f0b
Mohamed Nabil Mohamed Abusarea <mnbil0680@gmail.com>
2025-07-09 01:12:30 +0300

    add Day1 Task2 Even or Odd Program

---
+++ D1_T2_EvenOrOdd/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace D1_T2_EvenOrOdd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a C# program that checks if a number entered by the user is even or odd
+            //Created by Mohamed Nabil
+            Console.Write("Please Enter a Number: ");
+            int Numebr = int.Parse(Console.ReadLine());
+            if (Numebr % 2 == 0)
+            {
+                Console.WriteLine("Your Number Is Even");
+            }
+            else
+            {
+                Console.WriteLine("Your Number Is Odd");
+            }
+        }
+    }
+}
