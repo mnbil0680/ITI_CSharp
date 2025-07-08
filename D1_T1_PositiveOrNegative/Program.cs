18730ee91a3cddf614dfe607d419d70100726e04
Mohamed Nabil Mohamed Abusarea <mnbil0680@gmail.com>
2025-07-09 00:41:22 +0300

    add task 1 check number Positive or Negative

---
+++ D1_T1_PositiveOrNegative/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace D1_T1_PositiveOrNegative
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Write a C# program that checks if a number entered by the user is positive, negative
+            // Created By Mohamwd Nabil
+
+            Console.Write("Please Enter a Number: ");
+            int Number = int.Parse(Console.ReadLine());
+            if (Number > 0)
+            {
+                Console.WriteLine("Positive Number +");
+            }
+            else if (Number < 0)
+            {
+                Console.WriteLine("Negative Number -");
+            }
+            else
+            {
+                Console.WriteLine("Your Number Is Zero 0");
+            }
+            
+        }
+    }
+}
